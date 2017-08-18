0116d6b684dc1c7051dcccc97cb83324d9cb2b7e
Cody Jablonski <Cody Jablonski>
2017-08-17 19:58:34 -0500

    get basic call from client to server happening
    added support for doing ng build, dotnet run workflow

--- halo.client/src/app/app.component.ts
+++ halo.client/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'app';
+  apiValues: string[] = [];
+
+  constructor(private _http: Http) { }
+
+  ngOnInit(): void {
+    this._http.get("/api/values")
+          .subscribe(resp => this.apiValues = resp.json() as string[])
+  }
 }
--- halo.server/Startup.cs
+++ halo.server/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System.IO;
 
 namespace halo.server
 {
@@ -34,7 +35,19 @@ namespace halo.server
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.Use(async (context, next) => {
+                await next();
+                if (context.Response.StatusCode == 404 &&
+                        !Path.HasExtension(context.Request.Path.Value) &&
+                        !context.Request.Path.Value.StartsWith("/api/")) {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+            });
+
+            app.UseMvcWithDefaultRoute();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
